82d5a23a85866938f578eb91e11a0394b9541215
unknown <40111906@ME0MC01-140145.napier-mail.napier.ac.uk>
2015-11-18 09:58:42 +0000

    Demo Done

--- AnDS cw/src/com/grant/smith/Main.java
+++ AnDS cw/src/com/grant/smith/Main.java
@@ -11,8 +11,7 @@ public class Main {
 	static String resultsFolder = "Results/";
 	
 	public static void main(String[] args) {
-		
-		try{ //VRClarkeWrightSolution
+		try{
 
 			// Load all files in the test data folder that end with prob.csv and don't contain fail
 			Pattern problemFilePattern = Pattern.compile("prob\\.csv");
@@ -30,25 +29,30 @@ public class Main {
 			int fileNo = 0;
 			// For each file, run the VRP solver
 			for(File f : files){
+				// Solution classes:
+				//	VRClarkeWrightSolution
+				//	VRPartitionSolution
 				VRPartitionSolution vrS = new VRPartitionSolution(new VRProblem(testDataFolder + f.getName()));
 				
-				double loops = 100;
-				long startTime = System.currentTimeMillis();
+				double loops = 1;
+				long startTime = System.nanoTime();
 				for(int i = 0; i < loops; i++){
 					vrS.solve();
 				}
 				// store the time taken for the total loops
-				times[fileNo] = (int)(System.currentTimeMillis() - startTime);
+				times[fileNo] = (int)((System.nanoTime() - startTime)/1000000.0);
 
 				String fileName = f.getName().substring(0, f.getName().length() - "prob.csv".length());
-				System.out.println(fileName);
+				
 				
 				// output information about solution to the console
+				System.out.println("-------------"+fileName+"-------------");
 				System.out.println("Time taken for " + loops + " loops = " + times[fileNo] + "ms");
 				System.out.println("Avg time " + (int)(times[fileNo]/loops) + "ms");
 				System.out.println("Number of routes = " + vrS.soln.size());
 				System.out.println("Cost = " + vrS.solnCost());
 				System.out.println("vrS.verify() returned " + vrS.verify());
+				System.out.println();
 				
 				// output the solution
 				vrS.writeSVG(resultsFolder +  fileName + "prob.svg", resultsFolder + fileName + "solu.svg");
